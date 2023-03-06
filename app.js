9b79b577ddb6a1b893299ad3c1b459d801610feb
PrathameshLakare <prathameshlakare001@gmail.com>
2023-03-06 18:46:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post("/", function (req, re) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members:[
+        {
+            email_address: email,
+            status: "subscribed",
+            merge_fields:{
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/30efb08666";
+  const options = {
+    method: "POST",
+    auth: "prathamesh1:eb00d86385bd5cd3c82224e51ea2cfc5-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    
+    if(response.statusCode === 200){
+        re.sendFile(__dirname + "/success.html");
+    }else{
+        re.sendFile(__dirname + "/failure.html");
+    }
+    
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+//redirecting to home by pressing try again butn of failure page
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is working at port 3000");
+  })
+
+
+// list id 30efb08666
